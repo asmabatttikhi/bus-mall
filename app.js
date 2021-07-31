42eeab96e8511df22c460b44508bd1ecbb3b9d13
asmabatttikhi <asmaaalbattikhi@gmail.com>
2021-07-31 22:11:22 +0300

    charts

--- app.js
+++ app.js
@@ -2,46 +2,47 @@
 //make object container for img to show three of them
 
 
-Product.allproduct=[];
+Product.allproduct = [];
+var chart;
 
-function Product(nameproduct,srcurl) {
-  this.nameproduct= nameproduct;
-  this.srcurl= srcurl;
-  this.votes=0;
-  this.shown=0;
+function Product(nameproduct, srcurl) {
+  this.nameproduct = nameproduct;
+  this.srcurl = srcurl;
+  this.votes = 0;
+  this.shown = 0;
   Product.allproduct.push(this);
 }
 
-let btn=document.getElementById('button');
+let btn = document.getElementById('button');
 
-let leftimage=document.getElementById('left-image');
-let middleimage=document.getElementById('middle-image');
-let rightimage=document.getElementById('right-image');
+let leftimage = document.getElementById('left-image');
+let middleimage = document.getElementById('middle-image');
+let rightimage = document.getElementById('right-image');
 
-let maxAttempts=25;
-let userAttemptsCounter=0;
+let maxAttempts = 25;
+let userAttemptsCounter = 0;
 
 
 
-new Product('bag','images/bag.jpg');//0
-new Product('banana','images/banana.jpg');//1
-new Product('bathroom','images/bathroom.jpg');//2
-new Product('boots','images/boots.jpg');//3
-new Product('breakfast','images/breakfast.jpg');//4
-new Product('bubblegum','images/bubblegum.jpg');//5
-new Product('chair','images/chair.jpg');//6
-new Product('cthilhu','images/cthulhu.jpg');//7
-new Product('dog-duck','images/dog-duck.jpg');//8
-new Product('dragon','images/dragon.jpg');//9
-new Product('pen','images/pen.jpg');//10
-new Product('pet-sweep','images/pet-sweep.jpg');//11
-new Product('scissors','images/scissors.jpg');//12
-new Product('shark','images/shark.jpg');//13
-new Product('sweep','images/sweep.png');//14
-new Product('tauntaun','images/tauntaun.jpg');//15
-new Product('unicorn','images/unicorn.jpg');//16
-new Product('water-can','images/water-can.jpg');//17
-new Product('wine-glass','images/wine-glass.jpg');//18
+new Product('bag', 'images/bag.jpg');//0
+new Product('banana', 'images/banana.jpg');//1
+new Product('bathroom', 'images/bathroom.jpg');//2
+new Product('boots', 'images/boots.jpg');//3
+new Product('breakfast', 'images/breakfast.jpg');//4
+new Product('bubblegum', 'images/bubblegum.jpg');//5
+new Product('chair', 'images/chair.jpg');//6
+new Product('cthilhu', 'images/cthulhu.jpg');//7
+new Product('dog-duck', 'images/dog-duck.jpg');//8
+new Product('dragon', 'images/dragon.jpg');//9
+new Product('pen', 'images/pen.jpg');//10
+new Product('pet-sweep', 'images/pet-sweep.jpg');//11
+new Product('scissors', 'images/scissors.jpg');//12
+new Product('shark', 'images/shark.jpg');//13
+new Product('sweep', 'images/sweep.png');//14
+new Product('tauntaun', 'images/tauntaun.jpg');//15
+new Product('unicorn', 'images/unicorn.jpg');//16
+new Product('water-can', 'images/water-can.jpg');//17
+new Product('wine-glass', 'images/wine-glass.jpg');//18
 
 //console.log(Product);
 
@@ -54,30 +55,37 @@ function getRandomIndex() {
 let leftImageIndex;
 let middleImageIndex;
 let rightImageIndex;
+let pastimage = [];
 
+let namesArr = [];
+
+let votesArr = [];
+
+let shownArr = [];
 
 function renderThreeProducts() {
 
-  leftImageIndex=getRandomIndex();
-  middleImageIndex=getRandomIndex();
-  rightImageIndex=getRandomIndex();
+  leftImageIndex = getRandomIndex();
+  middleImageIndex = getRandomIndex();
+  rightImageIndex = getRandomIndex();
 
 
-  while (leftImageIndex===rightImageIndex||leftImageIndex===middleImageIndex||middleImageIndex===leftImageIndex||middleImageIndex===rightImageIndex) {
-    rightImageIndex=getRandomIndex();
-    leftImageIndex=getRandomIndex();
-    middleImageIndex=getRandomIndex();
+  while (leftImageIndex === rightImageIndex || leftImageIndex === middleImageIndex || middleImageIndex === leftImageIndex || middleImageIndex === rightImageIndex || pastimage.includes(leftImageIndex) || pastimage.includes(middleImageIndex) || pastimage.includes(rightImageIndex)) {
+    rightImageIndex = getRandomIndex();
+    leftImageIndex = getRandomIndex();
+    middleImageIndex = getRandomIndex();
   }
 
+  pastimage = [rightImageIndex, leftImageIndex, middleImageIndex];
 
-  leftimage.src=Product.allproduct[leftImageIndex].srcurl;
+  leftimage.src = Product.allproduct[leftImageIndex].srcurl;
   Product.allproduct[leftImageIndex].shown++;
 
 
-  middleimage.src=Product.allproduct[middleImageIndex].srcurl;
+  middleimage.src = Product.allproduct[middleImageIndex].srcurl;
   Product.allproduct[middleImageIndex].shown++;
 
-  rightimage.src=Product.allproduct[rightImageIndex].srcurl;
+  rightimage.src = Product.allproduct[rightImageIndex].srcurl;
   Product.allproduct[rightImageIndex].shown++;
 
 
@@ -89,17 +97,17 @@ renderThreeProducts();
 // middleimage.addEventListener('click',handleUserClick);
 // rightimage.addEventListener('click',handleUserClick);
 
-let imagesDiv=document.getElementById('images-div');
-imagesDiv.addEventListener('click',handleUserClick);
+let imagesDiv = document.getElementById('images-div');
+imagesDiv.addEventListener('click', handleUserClick);
 
 function handleUserClick(event) {
 
   //console.log(event.target.id);
 
-  if (userAttemptsCounter<maxAttempts) {
+  if (userAttemptsCounter < maxAttempts) {
 
 
-    if (event.target.id==='left-image') {
+    if (event.target.id === 'left-image') {
 
       Product.allproduct[leftImageIndex].votes++;
       //console.log(Product.allproduct[leftImageIndex]);
@@ -107,39 +115,105 @@ function handleUserClick(event) {
       userAttemptsCounter++;
       renderThreeProducts();
 
-    }else if(event.target.id==='middle-image'){
+    } else if (event.target.id === 'middle-image') {
       Product.allproduct[middleImageIndex].votes++;
       renderThreeProducts();
       userAttemptsCounter++;
 
     }
-    else if(event.target.id==='right-image'){
+    else if (event.target.id === 'right-image') {
       Product.allproduct[rightImageIndex].votes++;
       renderThreeProducts();
       userAttemptsCounter++;
     }
-    else{
+    else {
       alert('click on the image');
     }
 
 
-  }else{
+  } else {
 
-    btn.hidden=false;
-    btn.addEventListener('click',listmaker);
-    imagesDiv.removeEventListener('click',handleUserClick);
+    btn.hidden = false;
+    btn.addEventListener('click', listmaker);
+    imagesDiv.removeEventListener('click', handleUserClick);
   }
+  chart.destroy();
+  showChart();
 }
 
 
 
 
-function listmaker(){
-  let list= document.getElementById('result-list');
+function listmaker() {
+  let list = document.getElementById('result-list');
   for (let i = 0; i < Product.allproduct.length; i++) {
     let listItem = document.createElement('li');
     list.appendChild(listItem);
-    listItem.textContent=`${Product.allproduct[i].nameproduct} has ${Product.allproduct [i].votes} votes`;
+    listItem.textContent = `${Product.allproduct[i].nameproduct} has ${Product.allproduct[i].votes} votes`;
   }
-  btn.removeEventListener('click',listmaker);
+  btn.removeEventListener('click', listmaker);
+}
+
+function refreshdata(){
+  votesArr=[];
+  shownArr=[];
+  namesArr=[];
+  for (let i = 0; i < Product.allproduct.length; i++) {
+    votesArr.push(Product.allproduct[i].votes);
+    shownArr.push(Product.allproduct[i].shown);
+    namesArr.push(Product.allproduct[i].nameproduct);
+  }
+
+}
+refreshdata();
+showChart();
+function showChart() {
+  refreshdata();
+  console.log(votesArr);
+  let ctx =  document.getElementById('myChart');
+
+  const data = {
+    labels: namesArr,
+    datasets: [
+      {
+        label: 'votes',
+        data: votesArr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+        ],
+        borderWidth: 1
+      },
+      {
+        label: 'Shown',
+        data: shownArr,
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgb(153, 102, 255)',
+        ],
+        borderWidth: 1
+      }
+
+    ]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+  chart =new Chart(
+    ctx,
+    config
+  );
 }
